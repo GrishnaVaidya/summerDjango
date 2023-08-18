afb53977d423dff04bfa694cc34bd7d0e69e32b4
GrishnaVaidya <np03cs4s220237@heraldcollege.edu.np>
2023-08-18 09:10:40 +0545

    dsfdsff

---
+++ summer-django/crud/views.py
@@ -0,0 +1,89 @@
+from django.shortcuts import render,redirect
+from .models import Blog,Contact #manager->objects
+from django.core.paginator import Paginator
+from myproject.settings import EMAIL_HOST_USER #replace root with your project name
+from django.core.mail import EmailMessage
+from django.contrib.auth.decorators import login_required
+from django.template.loader import render_to_string
+
+
+
+from .forms import BlogForm
+
+# Create your views here.
+def homepage(request):
+    blog=Blog.objects.all()
+    paginator = Paginator(blog, 2)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    data = request.GET.get("search")
+    context = {
+        "blogs":page_obj,
+        "page_obj":page_obj
+    }
+    if(data is not "" and data is not None ):
+        searchData=Blog.objects.filter(title__contains=data)
+        print(searchData)
+        return render(request,"crud/index.html",{"blog":searchData})
+         
+
+    return render(request,"crud/index.html",context)
+
+def particularData(request,id):
+    blog=Blog.objects.get(id=id)
+    context ={"blog":blog}
+    return render(request,"crud/post.html",context)
+
+@login_required
+def create(request):
+    forms = BlogForm(request.POST or None)
+
+    if(forms.is_valid()):
+        forms.save()
+        return redirect("crud:home")
+
+    return render(request,"crud/creates.html",{"forms":forms})
+
+
+
+def contacts(request):
+    if(request.method=="POST"):
+        name=request.POST.get("name")
+        email=request.POST.get("email")
+        message=request.POST.get("message")
+        subject = "Wants to Collaborate!!!"
+        recipient = "np03cs4s220237@heraldcollege.edu.np",
+       
+        cont =Contact.create(name,message,email)
+        cont.save()
+        template = render_to_string('crud/email.html',{'name':name,'description':message,'mail':email})
+        email=EmailMessage(subject,template,EMAIL_HOST_USER,recipient)
+
+
+        email.fail_silently=False
+        if email!=None:
+            email.send()
+
+        
+    return render(request,"crud/contact.html")
+
+def deleteData(request,id):
+    blog=Blog.objects.get(id=id)
+    blog.delete()
+    return redirect("crud:home")
+
+def updateData(request,id):
+    blog=Blog.objects.get(id=id)
+    forms=BlogForm(request.POST or None,instance=blog)
+    context = {
+        "forms":forms,
+        "title":blog.title,
+        "subtitle":blog.subtitle,
+        "description":blog.description
+    }
+    if forms.is_valid():
+        forms.save()
+        return redirect("crud:home")
+
+    return render(request,"crud/creates.html",context)
+   
---
+++ summer-django/users/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import logout,authenticate,login
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.ma
+def registerUser(request):
+    if request.method == "POST":
+        username=request.POST.get("username")
+        email=request.POST.get("email")
+        firstname=request.POST.get("firstname")
+        lastname=request.POST.get("lastname")
+        password=request.POST.get("password")
+        confirmpassword=request.POST.get("confirmpassword")
+       
+        if password==confirmpassword:
+            users = User.objects.create_user(
+                username=username,
+                first_name=firstname,
+                last_name=lastname,
+                email=email,
+                password=password
+        )
+        users.save()
+        return redirect("crud:home")
+       
+    return render(request,"users/register.html")
+
+
+
+
+def loginUser(request):
+    if not request.user.is_authenticated:
+        if request.method=="POST":
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            user = authenticate(request,username=username,password=password)
+
+            if user is not None:
+                login(request,user)
+                return redirect("crud:home")
+            else:
+                return redirect("users:signin")
+        return render(request,"users/login.html")
+    else:
+        return redirect("crud:home")
+
+@login_required
+def logoutUser(request):
+    logout(request)
+    return redirect("crud:home")
